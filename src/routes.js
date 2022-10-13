e596cf3e9f594798d64392c0b1127e1bd51a4697
Carlos Matheus <cmsouza1998@gmail.com>
2022-10-13 09:40:13 -0300

    Router DOM Hooks

--- src/routes.js
+++ src/routes.js
@@ -1,20 +1,26 @@
 import Menu from "components/Menu";
+import Footer from "components/Footer";
 import Inicio from "pages/Inicio";
 import Cardapio from "pages/Cardapio";
+import NotFound from "pages/NotFound";
+import Sobre from "pages/Sobre";
 import {BrowserRouter as Router, Routes, Route} from "react-router-dom";
 import PaginaPadrao from "components/PaginaPadrao";
 
 export default function AppRouter(){
     return(
-        <main>
+        <main className='container'>
             <Router>
                 <Menu />
                 <Routes>
                     <Route path="/" element={<PaginaPadrao />}>
                         <Route index element={<Inicio />} />
                         <Route path="cardapio" element={<Cardapio />} />
+                        <Route path="sobre" element={<Sobre />} />
                     </Route>
+                    <Route path="*" element={<NotFound />} />
                 </Routes>
+                <Footer />
             </Router>
         </main>
     );
