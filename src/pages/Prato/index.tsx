3e6b02598b8eaecf0fb38c6ed398fda9906eca83
Carlos Matheus <cmsouza1998@gmail.com>
2022-10-17 08:04:31 -0300

    NotFound

--- src/pages/Prato/index.tsx
+++ src/pages/Prato/index.tsx
@@ -1,37 +1,45 @@
 import styles from "./Prato.module.scss";
-import {useParams, useNavigate} from "react-router-dom";
+import { useParams, useNavigate, Routes, Route } from "react-router-dom";
 import cardapio from "data/cardapio.json";
 import TagsPrato from "components/TagsPrato";
+import NotFound from "pages/NotFound";
+import PaginaPadrao from "components/PaginaPadrao";
 
-export default function Prato(){
-    const {id} = useParams();
+export default function Prato() {
+    const { id } = useParams();
     const prato = cardapio.find(item => item.id === Number(id));
 
     const navigate = useNavigate();
 
-    if(!prato){
-        return "Não tem prato";
+    if (!prato) {
+        return <NotFound />;
     }
 
-    return(
-        <>
-            <button className={styles.voltar} onClick={() => navigate(-1)}>
-                {"< Voltar"}
-            </button>
-            <section className={styles.container}>
-                <h1 className={styles.titulo}>
-                    {prato.title}
-                </h1>
-                <div className={styles.imagem}>
-                    <img src={prato.photo} alt={prato.title} />
-                </div>
-                <div className={styles.conteudo}>
-                    <p className={styles.conteudo__descricao}>
-                        {prato.description}
-                        <TagsPrato {...prato} />
-                    </p>
-                </div>
-            </section>
-        </>
+    return (
+        <Routes>
+            <Route path="*" element={<PaginaPadrao />}>
+                <Route index element={
+                    <>
+                        <button className={styles.voltar} onClick={() => navigate(-1)}>
+                            {"< Voltar"}
+                        </button>
+                        <section className={styles.container}>
+                            <h1 className={styles.titulo}>
+                                {prato.title}
+                            </h1>
+                            <div className={styles.imagem}>
+                                <img src={prato.photo} alt={prato.title} />
+                            </div>
+                            <div className={styles.conteudo}>
+                                <p className={styles.conteudo__descricao}>
+                                    {prato.description}
+                                    <TagsPrato {...prato} />
+                                </p>
+                            </div>
+                        </section>
+                    </>
+                } />
+            </Route>
+        </Routes>
     );
 }
